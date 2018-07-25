70e12d03f24b247e3e154a4dd9e7361f287c0be9
dianbian <98657837@qq.com>
2018-07-25 20:09:54 +0800

    implicit_cast is safe

--- baseCom/Logging.h
+++ baseCom/Logging.h
@@ -5,4 +5,21 @@
 #include "LogStream.h"
 #include "Timestamp.h"
 
+class TimeZone;
+
+class Logger
+{
+ public:
+  enum LogLevel
+  {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    FATAL,
+    NUM_LOG_LEVELS,
+  };  
+};
+
 #endif //BASECOM_LOGGING_H
